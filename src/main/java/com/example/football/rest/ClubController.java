9af93d0f8140483f42e5da51094035e64cb767e0
Tmh3101 <minhhieu31012004@gmail.com>
2024-02-29 11:12:54 +0700

    fix club controller

--- src/main/java/com/example/football/rest/ClubController.java
+++ src/main/java/com/example/football/rest/ClubController.java
@@ -1,9 +1,7 @@
 package com.example.football.rest;
 
 import com.example.football.entity.Club;
-import com.example.football.entity.Leagues;
 import com.example.football.service.ClubService;
-import com.example.football.service.LeaguesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +11,9 @@ import org.springframework.web.bind.annotation.*;
 public class ClubController {
 
     private final ClubService clubService;
-    private final LeaguesService leaguesService;
 
-    public ClubController(@Autowired ClubService clubService, @Autowired LeaguesService leaguesService){
+    public ClubController(@Autowired ClubService clubService){
         this.clubService = clubService;
-        this.leaguesService = leaguesService;
     }
 
     @GetMapping("")
@@ -32,20 +28,17 @@ public class ClubController {
 
     @PostMapping("/addClub")
     public ResponseEntity<?> addClub(@RequestBody Club club){
-        club.setLeagues(leaguesService.getLeaguesById(club.getLeagues().getLeaguesId()));
         return ResponseEntity.ok(clubService.addClub(club));
     }
 
     @PostMapping("/updateClub")
     public ResponseEntity<?> updateClub(@RequestBody Club club) {
-        club.setLeagues(leaguesService.getLeaguesById(club.getLeagues().getLeaguesId()));
         return ResponseEntity.ok(clubService.updateClub(club));
     }
 
     @PostMapping("/deleteClub")
-    public ResponseEntity<?> deleteClub(@RequestBody Club club){
-        club.setLeagues(leaguesService.getLeaguesById(club.getLeagues().getLeaguesId()));
-        clubService.addClub(club);
+    public ResponseEntity<?> deleteClub(@RequestBody int id){
+        clubService.deleteClub(id);
         return ResponseEntity.ok("Done!");
     }
 
